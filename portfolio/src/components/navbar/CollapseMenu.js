042807b627545889f80bcc5c03de7497e0851768
Krizel Minnema <krizelminnema@gmail.com>
2020-08-15 20:57:48 -0700

    adding links to collapsed menu

--- portfolio/src/components/navbar/CollapseMenu.js
+++ portfolio/src/components/navbar/CollapseMenu.js
@@ -1,2 +1,59 @@
-import React from 'react'
-import styled from 'styled-components'
+import { useSpring, animated } from 'react-spring';
+
+const CollapseMenu = (props) => {
+  const { open } = useSpring({ open: props.navbarState ? 0 : 1 });
+
+  if (props.navbarState === true) {
+    return (
+      <CollapseWrapper style={{
+        transform: open.interpolate({
+          range: [0, 0.2, 0.3, 1],
+          output: [0, -20, 0, -200],
+        }).interpolate(openValue => `translate3d(0, ${openValue}px, 0`),
+      }}
+      >
+        <Nav>
+          <li><a href='/' onClick={props.handleNavbar}>01. about</a></li>
+          <li><a href='/' onClick={props.handleNavbar}>02. experience</a></li>
+          <li><a href='/' onClick={props.handleNavbar}>03. featured works</a></li>
+          <li><a href='/' onClick={props.handleNavbar}>04. contact</a></li>
+          {/* Need to include button to highlight CV */}
+        </Nav>
+      </CollapseWrapper>
+    );
+  }
+  return null;
+};
+
+export default CollapseMenu;
+
+const CollapseWrapper = styled(animated.div)`
+  background: #2d3436;
+  position: fixed;
+  top: 4.5rem;
+  left: 0;
+  right: 0;
+`;
+
+const Nav = styled.ul`
+  list-style-type: none;
+  padding: 2rem 1rem 2rem 2rem;
+
+  & li {
+    transition: all 300ms linear 0s;
+  }
+
+  & a {
+    font-size: 1.4rem;
+    line-height: 2;
+    color: #dfe6e9;
+    text-transform: uppercase;
+    text-decoration: none;
+    cursor: pointer;
+
+    &:hover {
+      color: #fdcb6e;
+      border-bottom: 1px solid #fdcb6e;
+    }
+  }
+`;
