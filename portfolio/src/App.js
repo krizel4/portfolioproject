a1536666d7b852a4c166af4ec7d1966f27c8eece
Krizel Minnema <krizelminnema@gmail.com>
2020-08-15 20:42:16 -0700

    adding logo information to brand

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import MainContent from './components/index';
+import Navbar from './components/navbar/Navbar';
+import GlobalStyle from './styles/Global';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      <MainContent />
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    navbarOpen: false
+  }
+
+  handleNavbar = () => {
+    this.setState({ navbarOpen: !this.state.navbarOpen });
+  }
+
+  render() {
+
+    return (
+      <>
+        <Navbar
+          navbarState={this.state.navbarOpen}
+          handleNavbar={this.handleNavbar}
+        />
+        <GlobalStyle />
+        <MainContent />
+        </>
+    )
+  }
 }
 
-export default App;
+export default App
