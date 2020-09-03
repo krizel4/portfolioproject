183e12bd5783d812388d058fce8888c8f9396717
Krizel Minnema <krizelminnema@gmail.com>
2020-09-02 22:40:09 -0700

    fixed contact form. was missing end tags. adding some featured work

--- portfolio/src/components/Work.js
+++ portfolio/src/components/Work.js
@@ -1,12 +1,75 @@
-import React from 'react'
+import React from "react";
 
 function FeaturedWork() {
-    return (
-        <div id='featured-works'>
-            <h1>Featured Work</h1>
-            <p>Sint hammock minim exercitation tbh. Anim locavore wayfarers adaptogen meh. Aute 3 wolf moon poke waistcoat crucifix vaporware. Green juice neutra bushwick microdosing raclette 3 wolf moon butcher af beard labore. Anim in four loko, craft beer labore subway tile before they sold out quis iPhone sint pickled direct trade jianbing enim.</p>
-        </div>
-    )
+  return (
+    <div id="featured-works">
+      <h1>Featured Work</h1>
+      <ul>
+        <li>
+          <i class="fa-li text-warning"></i>
+          <a href="https://github.com/krizel4/CASTR" target="_blank">
+            CASTR
+          </a>
+        </li>
+
+        <li>
+          <i class="fa-li text-warning"></i>
+          <a href="https://github.com/krizel4/burger" target="_blank">
+            Eat-da-Burger
+          </a>
+        </li>
+
+        <li>
+          <i class="fa-li text-warning"></i>
+          <a href="https://krizel4.github.io/liri-node-app/" target="_blank">
+            LIRI Bot
+          </a>
+        </li>
+
+        <li>
+          <i class="fa-li text-warning"></i>
+          <a href="https://github.com/krizel4/Muzic-Xpansion" target="_blank">
+            muzicXpansion
+          </a>
+        </li>
+
+        <li>
+          <i class="fa-li text-warning"></i>
+          <a href="https://github.com/krizel4/TrainTime" target="_blank">
+            Train Time
+          </a>
+        </li>
+
+        <li>
+          <i class="fa-li text-warning"></i>
+          <a href="https://github.com/krizel4/week6-giphy" target="_blank">
+            Giphy API
+          </a>
+        </li>
+
+        <li>
+          <i class="fa-li text-warning"></i>
+          <a href="https://github.com/krizel4/TriviaGame" target="_blank">
+            Trivia Game
+          </a>
+        </li>
+
+        <li>
+          <i class="fa-li text-warning"></i>
+          <a href="https://github.com/krizel4/unit-4-game" target="_blank">
+            Crystal Game
+          </a>
+        </li>
+
+        <li>
+          <i class="fa-li text-warning"></i>
+          <a href="https://github.com/krizel4/Psychic-Game" target="_blank">
+            Psychic Game
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
-export default FeaturedWork
+export default FeaturedWork;
