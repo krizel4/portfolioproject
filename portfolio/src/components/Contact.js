460f8c91f8d4fc93fcc3aefa357c0910394d2db7
Krizel Minnema <krizelminnema@gmail.com>
2020-09-02 22:09:03 -0700

    trying to debug contact section. clearing it out to start over

--- portfolio/src/components/Contact.js
+++ portfolio/src/components/Contact.js
@@ -3,13 +3,7 @@ import React from 'react';
 function ContactForm() {
   return (
     <div id='contact'>
-      <form>
-        <label>
-          Name:
-          <input type='text' name='name' />
-        </label>
-        <input type='submit' value='Submit' />
-      </form>
+
     </div>
   )
 }
